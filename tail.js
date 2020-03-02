2377f5f19b3f0026426fe273b67b69c8335da315
Adrian Faustino <contact.adrianfaustino@gmail.com>
2020-03-02 19:27:34 +0000

    Wrote tail function to return a new array with everything but its first element.

---
+++ tail.js
@@ -0,0 +1,35 @@
+const assertEqual = function(actual, expected) {
+  let a = actual;
+  let b = expected;
+  a = a.toString();
+  b = b.toString();
+  
+  if (a === b) {
+    return console.log(`Assertion Passed: [${a}] === [${b}]`);
+  } else {
+    return console.log(`Assertion Failed: [${a}] !== [${b}]`);
+  }
+};
+
+const tail = function(arr) {
+  if (arr.length <= 0) {
+    return;
+  } else if (arr.length === 1) {
+    return arr[0];
+  }
+
+  let newArr = [];
+  for (let i = 1; i < arr.length; i++) {
+    newArr.push(arr[i]);
+  }
+
+  console.log(newArr);
+  return newArr;
+};
+
+tail([1 , 2]);
+tail([1, 2, 3, 4]);
+tail(['Hello', 'World']);
+tail(['My', 'name', 'is']);
+
+assertEqual(tail([1, 2]), 2);
