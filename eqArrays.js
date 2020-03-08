1505b3c0be367787ff23d82b1d0657a846b4ef59
Adrian Faustino <contact.adrianfaustino@gmail.com>
2020-03-08 21:32:32 +0000

    Refactored eqArrays to use recursion

--- eqArrays.js
+++ eqArrays.js
@@ -9,20 +9,55 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+// const eqArrays = function(arr1, arr2) {
+//   if (arr1.length !== arr2.length) {
+//     return false;
+//   }
+
+//   for (let i = 0; i < arr1.length; i++) {
+//     if (arr1[i] !== arr2[i]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// };
+
 const eqArrays = function(arr1, arr2) {
+  let result;
+  let recursionResult;
+
   if (arr1.length !== arr2.length) {
     return false;
   }
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
+  
+  for (let index in arr1) {
+    if (Array.isArray(arr1[index])) {
+      recursionResult = eqArrays(arr1[index], arr2[index]);
+    } else if (arr1[index] === arr2[index]) {
+      result = true;
+    } else {
+      result = false;
     }
   }
-  return true;
+
+  if (recursionResult === undefined) {
+    return result;
+  } else {
+    return recursionResult;
+  }
 };
 
 // TEST
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
 assertEqual(eqArrays([1, 2, 3], [1, 2, '3']), false); // => should FAIL
 assertEqual(eqArrays([1 , 2], [1, 2, 3]), false); // => should FAIL
+
+
+const a = [1, 2, [3, [4]]];
+const b = [1, 2, [3, [4]]];
+const c = [1, 2, [3, [4, 5]]];
+
+console.log('======');
+
+assertEqual(eqArrays(a, b), true);
+assertEqual(eqArrays(c, b), false);
