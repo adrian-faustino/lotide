5b66b6d986bbd369fa9b4a8d91744f8a843a5368
Adrian Faustino <contact.adrianfaustino@gmail.com>
2020-03-08 21:20:49 +0000

    Refactored eqObjects to use recursion

--- eqObjects.js
+++ eqObjects.js
@@ -22,28 +22,95 @@ const eqArrays = function(arr1, arr2) {
   return true;
 };
 
+// const eqObjects = function(obj1, obj2) {
+//   const obj1_KEYS = Object.keys(obj1); // this is an array of keys
+//   const obj2_KEYS = Object.keys(obj2);
+
+//   if (obj1_KEYS.length !== obj2_KEYS.length) { //compare lengths
+//     return false;
+//   }
+
+//   for (let key of obj1_KEYS) {
+//     if (typeof obj1[key] )
+//     const value_1 = obj1[key]; //this is the value of each key
+//     const value_2 = obj2[key];
+
+//     if (Array.isArray(value_1) && Array.isArray(value_2)) { //if both are arrays, use eqArrays
+//       if (!eqArrays(value_1, value_2)) {
+//         return false;
+//       }
+//     } else if (value_1 !== value_2) {
+//       return false;
+//     }
+//   }
+
+//   return true;
+// };
+
+// const eqObjects2 = function(obj1, obj2) {
+//   const obj1_KEYS = Object.keys(obj1); // this is an array of keys
+//   const obj2_KEYS = Object.keys(obj2);
+//   console.log(typeof obj1);
+
+//   if (obj1_KEYS.length !== obj2_KEYS.length) { //compare lengths
+//     return false;
+//   }
+
+//   for (let key in obj1) {
+//     if (typeof obj1[key] === 'object') {
+//       eqObjects2(key, obj2[key]);
+//     } else if (obj1[key] === obj2[key]) {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   }
+// };
+
+// const eqObjects = function(obj1, obj2) {
+//   //compare number of keys in an object
+//   console.log(Object.keys(obj1).length + ' -> obj 1 length **' + Object.keys(obj2).length + ' -> Object 2 length');
+//   if (Object.keys(obj1).length !== Object.keys(obj2).length) {
+//     return false;
+//   }
+
+//   for (let key in obj1) {
+//     console.log(key);
+//     if (typeof obj1[key] === 'object') {
+//       console.log('recursion!');
+//       eqObjects(obj1[key], obj2[key]);
+//     } else if (obj1[key] !== obj2[key]) {
+//       return false;
+//     } 
+//   }
+  
+//   // return true;
+// };
+
 const eqObjects = function(obj1, obj2) {
-  const obj1_KEYS = Object.keys(obj1); // this is an array of keys
+  const obj1_KEYS = Object.keys(obj1);
   const obj2_KEYS = Object.keys(obj2);
 
-  if (obj1_KEYS.length !== obj2_KEYS.length) { //compare lengths
-    return false;;
+  let result;
+  let recursionResult;
+  if (obj1_KEYS.length !== obj2_KEYS.length) {
+    return false;
   }
 
-  for (let key of obj1_KEYS) {
-    const value_1 = obj1[key]; //this is the value of each key
-    const value_2 = obj2[key];
-
-    if (Array.isArray(value_1) && Array.isArray(value_2)) { //if both are arrays, use eqArrays
-      if (!eqArrays(value_1, value_2)) {
-        return false;
-      }
-    } else if (value_1 !== value_2) {
-      return false;
+  for (let key in obj1) {
+    if (typeof obj1[key] === 'object') {
+      recursionResult = eqObjects(obj1[key], obj2[key]);
+    } else if (obj1[key] === obj2[key]) {
+      result = true;
+    } else {
+      result = false;
     }
   }
-
-  return true;
+  if (recursionResult === undefined) {
+    return result;
+  } else {
+    return recursionResult;
+  }
 };
 
 // TEST CODE
@@ -60,3 +127,10 @@ console.log(eqObjects(cd, dc)); // => true
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
 console.log(eqObjects(cd, cd2)); // => false
+
+console.log('=====');
+
+console.log(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })); // => true
+
+console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })); // => false
+console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 })); // => false
